e42e3bda3e4ea1a1bc890885867c40a6a26719b2
Robert Flesch <robertflesch@yahoo.com>
2012-12-12 09:51:58 -0500

    created eventManager and eventObject to centralize event management

---
+++ Source/com/drinkzage/events/EventManager.hx
@@ -0,0 +1,48 @@
+﻿package com.drinkzage.events;
+
+import nme.events.EventDispatcher;
+
+import com.drinkzage.events.EventObject;
+
+/**
+ * @author Robert Flesch
+ */
+class EventManager
+{
+	private var _events:Array<EventObject> = null;
+	public function new () 
+	{
+		_events = new Array<EventObject>();
+	}
+	
+	public function addEvent( target:EventDispatcher, type:String, listener:Dynamic->Void, useCapture:Bool = false, add:Bool = false):Void 
+	{
+		trace( "EventManager.addEvent - target: " + target + "  type: " + type );
+		var eo:EventObject = new EventObject( target, type, listener, useCapture, add );
+		_events.push( eo );
+	}
+
+	public function removeEvent( target:EventDispatcher, type:String, listener:Dynamic->Void ):Void 
+	{
+		trace( "EventManager.removeEvent - target: " + target + "  type: " + type );
+		// can be slow if lots of listeners, but works for now
+		for ( e in 0..._events.length )
+		{
+			if ( _events[e]._target == target 
+			&& _events[e]._type == type
+			&& _events[e]._listener == listener )
+			{
+				_events[e].dispose();
+			}
+		}
+	}
+	
+	public function removeAllEvents():Void 
+	{
+		trace( "EventManager.removeAllEvents" );
+		while ( 0 < _events.length )
+		{
+			_events.pop().dispose();
+		}
+	}
+}
---
+++ Source/com/drinkzage/events/EventObject.hx
@@ -0,0 +1,34 @@
+﻿package com.drinkzage.events;
+
+import nme.events.Event;
+import nme.events.EventDispatcher;
+
+
+/**
+ * @author Robert Flesch
+ */
+
+class EventObject
+{
+	public var _target:EventDispatcher = null;
+	public var _type:String;
+	public var _listener:Dynamic->Void = null;
+	private var _useCapture:Bool = false;
+	private var _add:Bool = false;
+	
+	public function new( target:EventDispatcher, type:String, listener:Dynamic->Void, useCapture:Bool, add:Bool ):Void 
+	{
+		_target = target;
+		_type = type;
+		_listener = listener;
+		_useCapture = useCapture;
+		_add = add;
+		
+		target.addEventListener( type, listener, useCapture, 0, add );
+	}
+	
+	public function dispose():Void
+	{
+		_target.removeEventListener( _type, _listener );
+	}
+}
